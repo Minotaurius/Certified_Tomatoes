e3f744659f7f23324a709f441a136ff2e038bb74
KappaMustafa <steef136@gmail.com>
2022-07-11 14:30:28 -0500

    merging

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
-//     fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_lfgodn81")
-//     .then((data) => data.json())
-//     .then((data) => console.log(data));
-// };
-// omdbData();
 
 var chosenMovie;
-fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_lfgodn81")
+var chosenRating;
+fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_fz44i7xl")
         .then(function (res) {
           return res.json();
         })
@@ -15,15 +11,39 @@ fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_lfgodn81")
             var chosenMovie = rand.id;
             console.log(chosenMovie);
 
-            fetch(`https://imdb-api.com/en/API/Posters/k_lfgodn81/${chosenMovie}`)
+            fetch(`https://imdb-api.com/en/API/Posters/k_fz44i7xl/${chosenMovie}`)
             .then(function (res) {
                 return res.json();
               })
               .then(function (data) {
-                  console.log(data.posters[0]);
+                var chosenPoster = data.posters[0]
+                $('.movie-display').attr('src', chosenPoster.link);
+                  console.log(chosenPoster);
+                  
+              fetch(`https://imdb-api.com/en/API/MetacriticReviews/k_fz44i7xl/${chosenMovie}`)
+              .then(function (res) {
+                  return res.json();
+                })
+                .then(function (data) {
+                  var chosenReview = data.items[0].content
+                  $('.review-container').text(chosenReview);
+                    console.log(chosenReview);
+                    
+
+
+                    fetch(`https://imdb-api.com/en/API/Ratings/k_fz44i7xl/${chosenMovie}`)
+              .then(function (res) {
+                  return res.json();
+                })
+                .then(function (data) {
+                  chosenRating = data.rottenTomatoes
+                    console.log(chosenRating);
+  
+                })
+  
+                })
 
               })
-
 
 
 
