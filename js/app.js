0eda33410a14a9c6ae5550dad1289407668a44b7
JamesB <James.buchmann@gmail.com>
2022-07-11 14:40:26 -0500

    merging

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
-//     fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_lfgodn81")
-//     .then((data) => data.json())
-//     .then((data) => console.log(data));
-// };
-// omdbData();
+
+// first api k_lfgodn81
+// second api k_gg2341wi
+
 
 var chosenMovie;
-fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_lfgodn81")
+fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_gg2341wi")
         .then(function (res) {
           return res.json();
         })
         .then(function (data) {
+          // this is getting a random movie from an array of the top 200
             var array = data.items;
             var rand = array[Math.floor(Math.random() * data.items.length)];
+            // rand.id, id is the imdb number iniside the object.. inside the array
             var chosenMovie = rand.id;
             console.log(chosenMovie);
-
-            fetch(`https://imdb-api.com/en/API/Posters/k_lfgodn81/${chosenMovie}`)
+          // this is fetching the api. the chosenMovie variable is set the the random idmb ID which is required in the api link
+            fetch(`https://imdb-api.com/en/API/Posters/k_gg2341wi/${chosenMovie}`)
             .then(function (res) {
                 return res.json();
               })
               .then(function (data) {
-                  console.log(data.posters[0]);
+                // this is console logging the first poster in the array of posters.... the first poster is the best poster 
+                var chosenPoster = data.posters[0];
+                  console.log(chosenPoster);
+                  $('.movie-display').attr('src', chosenPoster.link);
 
+                  fetch("https://imdb-api.com/en/API/BoxOfficeAllTime/k_gg2341wi")
+                  .then(function (res) {
+                    return res.json();
+                  })
+                  .then(function (data) {
+
+                    
               })
 
 
